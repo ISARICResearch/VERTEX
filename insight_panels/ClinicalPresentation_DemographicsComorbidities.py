d5489b69425debcf9fd7b834c08594dad353bd90
Tom Edinburgh <45427287+tedinburgh@users.noreply.github.com>
2024-09-18 17:47:33 +0100

    Change graph_about

--- insight_panels/ClinicalPresentation_DemographicsComorbidities.py
+++ insight_panels/ClinicalPresentation_DemographicsComorbidities.py
@@ -12,53 +12,37 @@ import redcap_config as rc_config
 
 ############################################
 ############################################
-# Change only this section
+# SECTION 1: Change this section
 ############################################
 ############################################
 
+# The suffix must be unique to each insight panel, it is used by the dashboard
 suffix = 'DemogComor'
-panel_title = 'Demographics / Comorbidities'
+# Multiple insight panels can share the same research question
+# Insight panels are grouped in the dashboard menu according to this
 research_question = 'Clinical Presentation'
+# The combination of research question and clinical measure must be unique
+clinical_measure = 'Demographics / Comorbidities'
 
-instructions_str = '''
-1. Select/remove countries using the dropdown (type directly into the dropdowns to search faster).
-2. Change datasets using the dropdown (country selections are remembered).
-3. Hover mouse on chart for tooltip data.
-4. Zoom-in with lasso-select (left-click-drag on a section of the chart). To reset the chart, double-click on it.
-5. Toggle selected countries on/off by clicking on the legend (far right).
-'''
-
-about_str = '''
-Information about each visualisation in the insight panel
-1. ...
-2. ...
-'''
-
-# sections = ['dates', 'demog', 'comor', 'daily', 'outco']
-sections = ['dates', 'demog', 'daily', 'outco']
+# Provide a list of all ARCH data sections needed in the RAP dataframe
+# Only variables from these sections will appear in the visuals
+sections = ['dates', 'demog', 'comor', 'daily', 'outco']
 
 
 def create_visuals(df_map):
-    ############################################
-    # get Variable type
-    ############################################
+    '''
+    Create all visuals in the insight panel from the RAP dataframe
+    '''
     dd = getRC.getDataDictionary(redcap_url, redcap_api_key)
+    # variable_dict is a dictionary of lists according to variable type, which
+    # are: 'binary', 'date', 'number', 'freeText', 'units', 'categorical'
     variable_dict = getRC.getVariableType(dd)
-    variables_binary = variable_dict['binary']
-    # variables_date = variable_dict['date']
-    variables_number = variable_dict['date']
-    # variables_freeText = variable_dict['freeText']
-    # variables_units = variable_dict['units']
-    # variables_categorical = variable_dict['categorical']
 
-    # Variable and label dictionary
-    correct_names = dd[['field_name', 'field_label']]
-
+    # Population pyramid
     color_map = {
         'Discharge': '#00C26F',
         'Censored': '#FFF500',
         'Death': '#DF0069'}
-
     filter_columns = ['age_group', 'mapped_outcome', 'slider_sex']
     df_age_gender = df_map[filter_columns + ['usubjid']].groupby(
         filter_columns, observed=True).count().reset_index()
@@ -72,25 +56,64 @@ def create_visuals(df_map):
     pyramid_chart = idw.fig_dual_stack_pyramid(
         df_age_gender, base_color_map=color_map,
         graph_id='age_gender_pyramid_chart_' + suffix,
-        graph_label='Demographics: Population Pyramid')
-    print(df_age_gender.shape)
+        graph_label='Demographics: Population Pyramid',
+        graph_about='Dual-sided population pyramid, showing age, sex and outcome.')
 
+    # Demographics descriptive table
     demog_columns = [col for col in df_map.columns if col.startswith('demog')]
-    demog_columns += ['age']
+    demog_columns += ['age']  # This doesn't get renamed by correct_names?
     descriptive = ia.descriptive_table(
-        df_map[demog_columns], correct_names,
-        variables_binary, ['age'] + variables_number)
+        df_map[demog_columns],
+        correct_names=dd[['field_name', 'field_label']],
+        categoricals=variable_dict['binary'],
+        numericals=['age'] + variable_dict['number'])
     fig_table_symp = idw.fig_table(
         descriptive,
         graph_id='demog_table_' + suffix,
-        graph_label='Demographics: Descriptive Table')
+        graph_label='Demographics: Descriptive Table',
+        graph_about='Summary of demographics.')
 
-    return pyramid_chart, fig_table_symp
+    # Comorbodities frequency and upset charts
+    proportions_comor, set_data_comor = ia.get_proportions(
+        df_map, 'comorbidities')
+    freq_chart_comor = idw.fig_frequency_chart(
+        proportions_comor,
+        title='Frequency of comorbidities',
+        graph_id='comor_freq_' + suffix,
+        graph_label='Comorbidities: Frequency',
+        graph_about='Frequency of the ten most common comorbodities on presentation')
+    upset_plot_comor = idw.fig_upset(
+        set_data_comor,
+        title='Frequency of combinations of the five most common comorbidities',
+        graph_id='comor_upset_' + suffix,
+        graph_label='Comorbidities: Intersections',
+        graph_about='Frequency of combinations of the five most common comorbidities on presentation')
+
+    # Comorbidities descriptive table
+    comor_columns = [col for col in df_map.columns if col.startswith('comor')]
+    descriptive = ia.descriptive_table(
+        df_map[comor_columns],
+        correct_names=dd[['field_name', 'field_label']],
+        categoricals=variable_dict['binary'],
+        numericals=variable_dict['number'])
+    fig_table_comor = idw.fig_table(
+        descriptive,
+        graph_id='comor_table_' + suffix,
+        graph_label='Comorbidities: Descriptive Table',
+        graph_about='Summary of comorbodities.')
+
+    return pyramid_chart, fig_table_symp, freq_chart_comor, upset_plot_comor, fig_table_comor
 
 
 ############################################
 ############################################
-# REDCap elements
+# END SECTION 1: You should not need to change anything else
+############################################
+############################################
+
+############################################
+############################################
+# SECTION 2: Config, data reading, processing, modal creation
 ############################################
 ############################################
 
@@ -99,15 +122,12 @@ redcap_api_key = rc_config.redcap_api_key
 site_mapping = rc_config.site_mapping
 
 ############################################
-############################################
 # Data reading and initial proccesing
-############################################
 ############################################
 
 
 def create_df_map(redcap_url, redcap_api_key, site_mapping, sections):
-    vari_list = getRC.getVariableList(
-        redcap_url, redcap_api_key, sections)
+    vari_list = getRC.getVariableList(redcap_url, redcap_api_key, sections)
     df_map = getRC.get_REDCAP_Single_DB(
         redcap_url, redcap_api_key, site_mapping, vari_list)
 
@@ -147,10 +167,24 @@ for uniq_county in range(len(unique_countries)):
     country_dropdown_options.append(
         {'label': name_country, 'value': code_country})
 
-############################################
+# This text appears after clicking the insight panel's Instructions button
+instructions_str = '''
+1. Select/remove countries using the dropdown (type directly into the dropdowns to search faster).
+2. Change datasets using the dropdown (country selections are remembered).
+3. Hover mouse on chart for tooltip data.
+4. Zoom-in with lasso-select (left-click-drag on a section of the chart). To reset the chart, double-click on it.
+5. Toggle selected countries on/off by clicking on the legend (far right).
+'''
+
+# This text appears after clicking the insight panel's About button
+about_list = [
+    '<strong>' + label + '</strong>' + about
+    for _, label, about in create_visuals(df_map)]
+about_str = '\n'.join(
+    ['Information about each visual in the insight panel:'] + about_list)
+
 ############################################
 # Modal creation
-############################################
 ############################################
 
 
@@ -158,7 +192,14 @@ def generate_html_text(text):
     text_list = text.strip('\n').split('\n')
     div_list = []
     for line in text_list:
-        div_list.append(html.Div(line))
+        strong_list = line.split('<strong>')
+        for string in strong_list:
+            if '</strong>' in string:
+                strong, not_strong = string.split('</strong>')
+                div_list.append(html.Div(html.Strong(strong)))
+                div_list.append(html.Div(not_strong))
+            else:
+                div_list.append(html.Div(string))
         div_list.append(html.Br())
     div = html.Div(div_list[:-1])
     return div
@@ -169,25 +210,25 @@ def create_modal():
 
     modal = [
         dbc.ModalHeader(html.H3(
-            research_question + ': ' + panel_title,
-            id="line-graph-modal-title",
-            style={"fontSize": "2vmin", "fontWeight": "bold"})
+            research_question + ': ' + clinical_measure,
+            id='line-graph-modal-title',
+            style={'fontSize': '2vmin', 'fontWeight': 'bold'})
         ),
         dbc.ModalBody([
             dbc.Accordion([
                 dbc.AccordionItem(
-                    title="Filters and Controls",
+                    title='Filters and Controls',
                     children=[
                         idw.filters_controls(suffix, country_dropdown_options)]
                 ),
                 dbc.AccordionItem(
-                    title="Insights",
+                    title='Insights',
                     children=[
                         dbc.Tabs([
                             dbc.Tab(dbc.Row([
                                 dbc.Col(visual, id='col-'+visual.id)
                                 ]), label=label)
-                            for visual, label in visuals])
+                            for visual, label, _ in visuals])
                     ]
                 )
             ])
@@ -204,7 +245,7 @@ def create_modal():
 
 ############################################
 ############################################
-# Callbacks
+# SECTION 3: Callbacks
 ############################################
 ############################################
 
@@ -231,12 +272,12 @@ def register_callbacks(app, suffix):
 
         if trigger_id == f'country-selectall_{suffix}':
             if 'all' in select_all_value:
-                # "Select all" (now "Unselect all") is checked
+                # 'Select all' (now 'Unselect all') is checked
                 output = [
                     [option['value'] for option in all_countries_options],
                     [{'label': 'Unselect all', 'value': 'all'}], ['all']]
             else:
-                # "Unselect all" is unchecked
+                # 'Unselect all' is unchecked
                 output = [[], [{'label': 'Select all', 'value': 'all'}], []]
         elif trigger_id == f'country-checkboxes_{suffix}':
             if len(selected_countries) == len(all_countries_options):
@@ -256,9 +297,9 @@ def register_callbacks(app, suffix):
         return output
 
     @app.callback(
-        Output(f"country-fade_{suffix}", "is_in"),
-        [Input(f"country-display_{suffix}", "n_clicks")],
-        [State(f"country-fade_{suffix}", "is_in")]
+        Output(f'country-fade_{suffix}', 'is_in'),
+        [Input(f'country-display_{suffix}', 'n_clicks')],
+        [State(f'country-fade_{suffix}', 'is_in')]
     )
     def toggle_fade(n_clicks, is_in):
         state = is_in
@@ -273,7 +314,7 @@ def register_callbacks(app, suffix):
     )
     def update_country_display(selected_values, all_options):
         if not selected_values:
-            return "Country:"
+            return 'Country:'
 
         # Create a dictionary to map values to labels
         value_label_map = {
@@ -283,7 +324,7 @@ def register_callbacks(app, suffix):
         selected_labels = [
             value_label_map[val] for val in selected_values
             if val in value_label_map]
-        display_text = ", ".join(selected_labels)
+        display_text = ', '.join(selected_labels)
 
         if len(display_text) > 20:  # Adjust character limit as needed
             output = f'Country: {selected_labels[0]}, '
@@ -292,18 +333,9 @@ def register_callbacks(app, suffix):
             output = f'Country: {display_text}'
         return output
 
-    ############################################
-    ############################################
-    # Specific Callbacks
-    # Modify outputs
-    ############################################
-    ############################################
-
     @app.callback(
         [Output('col-' + visual.id, 'children')
-            for visual, _ in create_visuals(df_map)],
-        # [Output('age_gender_pyramid_chart_' + suffix, 'children'),
-        #  Output('demog_table_' + suffix, 'children')],
+            for visual, _, _ in create_visuals(df_map)],
         [Input(f'submit-button_{suffix}', 'n_clicks')],
         [State(f'gender-checkboxes_{suffix}', 'value'),
          State(f'age-slider_{suffix}', 'value'),
@@ -321,7 +353,7 @@ def register_callbacks(app, suffix):
         if filtered_df.empty:
             visuals = None
         else:
-            visuals = create_visuals(filtered_df)
+            visuals = [visual for visual, _, _ in create_visuals(filtered_df)]
         return visuals
 
     # End of callbacks
