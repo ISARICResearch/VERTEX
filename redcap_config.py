498d820f5958a9d75b7aebafd9c6ffce9a886ca9
Tom Edinburgh <45427287+tedinburgh@users.noreply.github.com>
2025-01-16 18:06:58 +0000

    Structural changes

--- redcap_config.py
+++ redcap_config.py
@@ -1,15 +1,16 @@
-redcap_api_key = 'BCC83FD05676798456262ED4883FD85C'  # Mpox synthetic data
-# redcap_api_key = '378073224101A9B362728EB629C0385F'  # Dengue synthetic data
+# redcap_api_key = 'BCC83FD05676798456262ED4883FD85C'  # Mpox synthetic data
+redcap_api_key = 'CD4820A24F88CE7892DD48E0F4743936'  # Dengue synthetic data
 redcap_url = 'https://ncov.medsci.ox.ac.uk/api/'
 
-site_mapping = {
-    '00000': 'COL',
-    '00001': 'GBR',
-    '00002': 'CAN',
-    '00003': 'KEN',
-    '00004': 'FRA',
-    '00005': 'POL',
-    '00006': 'UGA',
-    '00007': 'RWA',
-    '00008': 'BRA',
-    '00009': 'NLD'}
+# country_mapping = None
+# site_mapping = {
+#     '00000': 'COL',
+#     '00001': 'GBR',
+#     '00002': 'CAN',
+#     '00003': 'KEN',
+#     '00004': 'FRA',
+#     '00005': 'POL',
+#     '00006': 'UGA',
+#     '00007': 'RWA',
+#     '00008': 'BRA',
+#     '00009': 'NLD'}
