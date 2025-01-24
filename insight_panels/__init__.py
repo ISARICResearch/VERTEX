021aa6782d2087add93c9b8a99f1aee7732b7331
Tom <tom.edinburgh@ndm.ox.ac.uk>
2025-01-24 15:19:35 +0000

    Remove insight panel folder

--- insight_panels/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = [
-    # 'Examples_Generic',
-    'Enrolment_EnrolmentDetail',
-    'ClinicalPresentation_DemographicsComorbidities',
-    'Treatments_InterventionsTreatments',
-    'Outcomes_ComplicationsOutcomes'
-]
--- redcap_config.py
+++
@@ -1,4 +0,0 @@
-filepath='C:/Users/egarcia/OneDrive - Nexus365/Projects/PublicVERTEX/Global_Dengue/'
-config_file=eval(open(filepath+'config_file.txt', 'r').read())
-redcap_api_key = config_file['redcap_api_key']
-redcap_url=config_file['redcap_url']
