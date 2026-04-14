0b29d69ee783e229080621113b7fccd9a6cb6072
Sandeep Murthy <Sandeep.Murthy@ndm.ox.ac.uk>
2026-04-14 13:55:06 +0100

    refactor: changes to support loading of insight panels and buttons from modules that do not define `define_button` and `create_visuals` functions (ref. GitHub issue #81)

--- vertex/layout/insight_panels.py
+++ vertex/layout/insight_panels.py
@@ -24,22 +24,61 @@ def get_insight_panels(config_dict, insight_panels_path):
     insight_panels = {
         x: import_from_path(x, os.path.join(insight_panels_path, x + ".py")) for x in config_dict["insight_panels"]
     }
-    buttons = [{**ip.define_button(), **{"suffix": suffix}} for suffix, ip in insight_panels.items()]
+
+    # This is refactoring to allow insight panels and buttons to also be loaded
+    # from modules that do not have the `define_button` and `create_visuals`
+    # functions - modules in the existing format that do define these functions
+    # will still continue to work as before.
+    #
+    # See GitHub issue #81 for more information:
+    #
+    #     https://github.com/ISARICResearch/VERTEX/issues/81
+    try:
+        buttons = [{**ip.define_button(), **{"suffix": suffix}} for suffix, ip in insight_panels.items()]
+    except AttributeError:
+        buttons = [
+            {
+                **{"item": ip.RESEARCH_QUESTION_ITEM, "label": ip.RESEARCH_QUESTION_ITEM_LABEL},
+                **{"suffix": suffix}
+            }
+            for suffix, ip in insight_panels.items()
+        ]
+
     return insight_panels, buttons
 
 
 def get_visuals(buttons, insight_panels, df_map, df_forms_dict, dictionary, quality_report, filepath):
     for ii in range(len(buttons)):
         suffix = buttons[ii]["suffix"]
-        visuals = insight_panels[suffix].create_visuals(
-            df_map=df_map.copy(),
-            df_forms_dict={k: v.copy() for k, v in df_forms_dict.items()},
-            dictionary=dictionary.copy(),
-            quality_report=quality_report,
-            suffix=suffix,
-            filepath=filepath,
-            save_inputs=True,
-        )
+
+        # This is refactoring to allow insight panels and buttons to also be loaded
+        # from modules that do not have the `define_button` and `create_visuals`
+        # functions - modules in the existing format that do define these functions
+        # will still continue to work as before.
+        #
+        # See GitHub issue #81 for more information:
+        #
+        #     https://github.com/ISARICResearch/VERTEX/issues/81
+        try:
+            visuals = insight_panels[suffix].create_visuals(
+                df_map=df_map.copy(),
+                df_forms_dict={k: v.copy() for k, v in df_forms_dict.items()},
+                dictionary=dictionary.copy(),
+                quality_report=quality_report,
+                suffix=suffix,
+                filepath=filepath,
+                save_inputs=True,
+            )
+        except AttributeError:
+            visuals = insight_panels[suffix].main(
+                df_map=df_map.copy(),
+                df_forms_dict={k: v.copy() for k, v in df_forms_dict.items()},
+                dictionary=dictionary.copy(),
+                suffix=suffix,
+                filepath=filepath,
+                save_inputs=True
+            )
+
         buttons[ii]["graph_ids"] = [id for _, id, _, _ in visuals]
     return buttons
 
